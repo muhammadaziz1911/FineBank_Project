40971b74c083e2bc54f654432e5678406f72e8a7
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-06-02 22:22:58 +0500

    update

--- src/components/main/signup/Signup.jsx
+++ src/components/main/signup/Signup.jsx
@@ -1,11 +1,98 @@
 import React from 'react'
+import "./Signup.css"
+import { useState } from 'react';
+import finebank_avatar from "./../../../assets/main_images/finebank_avatar.png"
+
 
 const Signup = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div>
-      <h2>
-        Signup page
-      </h2>
+      
+      <div className="signup_page">
+
+        <div className="finebank_avatar">
+          <img src={finebank_avatar} alt="finebank_avatar" />
+        </div>
+
+        <div className="form_input">
+
+          <div className="email">
+            <h3>
+              Email Address
+            </h3>
+            <input type="email" placeholder='johndoe@gmail.com' />
+          </div>
+
+          <div className="password">
+            <div className="password_text">
+              <div className="password_h3">
+                <h3>Password</h3>
+              </div>
+              <div className="password_p">
+                <h3>Forgot password?</h3>
+              </div>
+            </div>
+
+            <div className="password_input_wrapper">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="●●●●●●●●●●●●●●"
+              />
+              <span
+                className="eye_icon"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  // Ko'z ochiq
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"          viewBox="0 0 24 24" fill="none" stroke="#4B5768"          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>
+                    <circle cx="12" cy="12" r="3"/>
+                  </svg>
+                ) : (
+                  // Ko'z yopiq
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"          viewBox="0 0 24 24" fill="none" stroke="#4B5768"          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7          0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94"/>
+                    <path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.          5 0 0 1-2.16 3.19"/>
+                    <line x1="1" y1="1" x2="23" y2="23"/>
+                  </svg>
+                )}
+              </span>
+            </div>
+          </div>
+
+          <label className="checkbox-label">
+            <input type="checkbox" id="keepSignedIn" />
+            <span className="custom-check"></span>
+            by ticking it , you agree terms of services
+          </label>
+
+          <div className="login_button">
+            <button type='submit'>
+              Sign up
+            </button>
+          </div>
+
+          <div className="line_signin">
+            <img src="" alt="line_signin" />
+          </div>
+
+          <div className="button_google">
+            <button type='submit'>Continue with Google</button>
+          </div>
+
+          <div className="link_create_account">
+            <p>
+              Already have an account? Sign in here
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }

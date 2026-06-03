3b5a02e9b72a783a55bcc943dd24d5121611093e
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-06-03 22:06:53 +0500

    update

---
+++ src/components/main/forgot_password/Forgot_password.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import finebank_avatar from "./../../../assets/main_images/finebank_avatar.png"
+
+const Forgot_password = () => {
+  return (
+    <div>
+      
+      <div className="forgot_password">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Forgot_password

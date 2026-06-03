4e24a3475e12678831a0df935fa23b92a7bec822
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-06-03 22:08:47 +0500

    update

--- src/components/main/forgot_password/Forgot_password.jsx
+++ src/components/main/forgot_password/Forgot_password.jsx
@@ -7,6 +7,20 @@ const Forgot_password = () => {
       
       <div className="forgot_password">
 
+        <div className="finebank_avatar">
+          <img src={finebank_avatar} alt="finebank_avatar" />
+        </div>
+
+        <div className="h3_password">
+          <h3>
+            Forgot Password?
+          </h3>
+
+          <p>
+            Enter your email adress to get the password reset link
+          </p>
+        </div>
+
       </div>
 
     </div>
